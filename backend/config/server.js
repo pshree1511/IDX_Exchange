58f5810e7a94362dbe4c64ee9ac7d480c0df9b38
pshree1511 <pshree1511@gmail.com>
2026-07-13 04:19:03 -0400

    add week 4
    Add request logging middleware and new route for properties

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,12 +4,52 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/api/health', require("./routes/health"));
+
+app.use((req, res, next) => {
+    const start = Date.now();
+
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+        const timestamp = new Date().toISOString();
+
+        console.log(
+            `[${timestamp}] ${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
+        );
+    });
+
+    next();
+});
+
+app.use("/api/health", require("../routes/health"));
+app.use("/api/properties", require("../routes/properties"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server running on port ${PORT}");
+    console.log(`Server running on port ${PORT}`);
 });
+
+// require("dotenv").config();
+
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use((req, res, next) =>{
+//     const timestamp = new Date().toISOString();
+//     console.log(`[${timestamp}] ${req.method} ${req.url}`);
+//     next();
+// });
+// app.use('/api/health', require("../routes/health"));
+// app.use('/api/properties", require("../routes/properties');
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
